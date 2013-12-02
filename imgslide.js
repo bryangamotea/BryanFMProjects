d68bdc7efbb4d701ce7f3a2f58cfa62917cc26f8
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-02 18:30:58 +0800

    Slider files added.

---
+++ imgslide.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+	$("#next").click(function(){
+		var my_margin = ($(".slider").css("margin-left"));
+		console.log(my_margin);
+		my_margin = my_margin.replace("px", "");
+		console.log(my_margin);
+
+		$(".slider").animate({"margin-left": (my_margin - 300) + "px"},1000, function(){
+			console.log($(this).css("margin-left"));
+
+		});
+	});
+});
