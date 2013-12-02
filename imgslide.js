4f278e559d805d5fc88a1277d5a230eda8a66e2d
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-02 20:14:05 +0800

    Fix next button.

--- imgslide.js
+++ imgslide.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
 		console.log(my_margin);
 		my_margin = my_margin.replace("px", "");
 		console.log(my_margin);
-
+		if (my_margin < 900){
 		$(".slider").animate({"margin-left": (my_margin - 300) + "px"},1000, function(){
 			console.log($(this).css("margin-left"));
-
+		} else 
+		{
+			
+		}
 		});
 	});
 });
