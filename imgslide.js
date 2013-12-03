2a33bb83655f4383b1dcf04ec81fd3fb54d39943
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-03 18:00:36 +0800

    50% of phase 2.

--- imgslide.js
+++ imgslide.js
@@ -3,30 +3,19 @@ $(document).ready(function() {
 
 	$("#next").click(function(){
 
-		
-		var my_margin = ($(".slider").css("margin-left"));
-		my_margin = my_margin.replace("px", "");
-		var next_margin = (my_margin - 300) + "px";
 
-		if (my_margin > -600) {
-			$(".slider").animate({"margin-left": next_margin},2000, function(){
-			
+
+			$(".slider").animate({"margin-left": "-100%"},500, function(){
+				$(".slider img:first-child").appendTo(".slider");
+				$(this).css("margin-left", "0px");
 			});
-		} 
-
-
 	});
 
 	$("#prev").click(function(){
 
-		var my_margin = ($(".slider").css("margin-left"));
-		my_margin = my_margin.replace("px", "");
-		var next_margin = (my_margin - -300) + "px";
-
-		if (my_margin < 0) {
-			$(".slider").animate({"margin-left": next_margin}, 2000, function(){
-			});
-		}
+			$(".slider img:last-child").prependTo(".slider");
+			$(".slider").css({"margin-left": "-100%"});		
+			$(".slider").animate({"margin-left": "0px"}, 500);
 
 	});
 
--- slider.js
+++ slider.js
@@ -3,31 +3,21 @@ $(document).ready(function() {
 
 	$("#right-but").click(function(){
 
-		
-		var my_margin = ($(".slider").css("margin-left"));
-		console.log(my_margin);
-		my_margin = my_margin.replace("px", "");
-		var next_margin = (my_margin - 650) + "px";
 
-		if (my_margin > -1000) {
-			$(".slider").animate({"margin-left": next_margin},500, function(){
-			
+			$(".slider").animate({"margin-left": "-100%"},500, function(){
+				$(".slider img:first-child").appendTo(".slider");
+				$(this).css("margin-left", "0px");
 			});
-		} 
-
-
 	});
 
 	$("#left-but").click(function(){
 
-		var my_margin = ($(".slider").css("margin-left"));
-		my_margin = my_margin.replace("px", "");
-		var next_margin = (my_margin - -650) + "px";
 
-		if (my_margin < 0) {
-			$(".slider").animate({"margin-left": next_margin}, 500, function(){
+			$(".slider img:last-child").prependTo(".slider");
+			$(".slider").css({"margin-left": "-100%"});		
+			$(".slider").animate({"margin-left": "0px"}, 500, function(){
+
 			});
-		}
 
 	});
 
