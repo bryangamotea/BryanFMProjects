4c55b96fe97cb6a96e6294e9f50757c15ef6003d
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-03 09:31:10 +0800

    Edit button positioning.

--- imgslide.js
+++ imgslide.js
@@ -2,17 +2,16 @@ $(document).ready(function() {
 
 
 	$("#next").click(function(){
+		
 		var my_margin = ($(".slider").css("margin-left"));
 		console.log(my_margin);
 		my_margin = my_margin.replace("px", "");
 		console.log(my_margin);
-		if (my_margin < 900){
+
 		$(".slider").animate({"margin-left": (my_margin - 300) + "px"},1000, function(){
 			console.log($(this).css("margin-left"));
-		} else 
-		{
-			
-		}
+
+
 		});
 	});
 });
