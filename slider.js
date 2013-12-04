fb3a01dec9bba2fcdec26c7ad3469c5a80533bf2
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-04 10:49:42 +0800

    Added pause/play/image counter to project.

--- slider.js
+++ slider.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+	
 
+	$("#pause-but").hide();
 
+	
 	$("#right-but").click(function(){
 
 
@@ -18,6 +21,44 @@ $(document).ready(function() {
 			$(".slider").animate({"margin-left": "0px"}, 500, function(){
 
 			});
+
+	});
+
+
+
+	$(".slider").tabs();
+
+	var TotImg = $(".slider > .banner").length
+	imgCounter(1,TotImg);
+
+	$(".slider").bind("tabselect", function(event, ui){
+		var index = ui.index;
+		var imgNum = index + 1;
+		imgCounter(imgNum, TotImg)
+	});
+
+	function imgCounter(imgNum, imgCounter) {
+		$("#counter").text(imgNum + "/" + TotImg);
+	}
+
+
+
+	$("#play-but").click(function() {
+		$("#pause-but").show(200);
+		$("#play-but").hide(200);
+
+		setTimeout(function(){
+			$(".slider").animate({"margin-left": "-100%"},500, function(){
+				$(".slider img:first-child").appendTo(".slider");
+				$(this).css("margin-left", "0px");
+			});
+		});
+
+	});
+
+	$("#pause-but").click(function() {
+		$("#pause-but").hide(200);
+		$("#play-but").show(200);
 
 	});
 
